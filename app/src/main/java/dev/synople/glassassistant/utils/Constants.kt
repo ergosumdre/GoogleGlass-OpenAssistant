0454352b7cb3d5b34a55e100895f775942f50b3d
Dre Dyson <35677602+ergosumdre@users.noreply.github.com>
2024-07-28 23:59:56 -0500

    Update Constants.kt

--- app/src/main/java/dev/synople/glassassistant/utils/Constants.kt
+++ app/src/main/java/dev/synople/glassassistant/utils/Constants.kt
@@ -3,8 +3,9 @@ package dev.synople.glassassistant.utils
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object GlassAssistantConstants {
-    val DATASTORE_OPEN_AI_API_KEY =
-        stringPreferencesKey("openAiApiKey")
+    val DATASTORE_OPEN_AI_API_KEY = stringPreferencesKey("openAiApiKey")
+    val DATASTORE_TAILSCALE_HOST_IP = stringPreferencesKey("tailscale_host_ip")
+
 
     val DEFAULT_PROMPT = "In one sentence, describe the primary object in this image."
 }
